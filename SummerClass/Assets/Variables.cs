c473374cc1e5f7e51f9917fadfd317345442fbd8
James Spencer <10483217@my.uvu.edu>
2021-06-09 22:37:10 -0600

    Op and Varssssss

---
+++ SummerClass/Assets/Variables.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VarHole : MonoBehaviour
+{
+    float potato = 30.40;
+    float sandwich = 340.23;
+    int bagcount = 10;
+}
