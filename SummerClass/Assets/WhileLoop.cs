872264c05ba70ea21c3dd9c6eefcf2479234ad39
James Spencer <10483217@my.uvu.edu>
2021-06-25 23:43:57 -0600

    Removed and added stuff so scripts would actually execute.

--- SummerClass/Assets/WhileLoop.cs
+++ SummerClass/Assets/WhileLoop.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LERP : MonoBehaviour
+public class WhileLoop : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
-
+        int i = 0;
+        while (i < 5)
+        {
+            Debug.Log(i);
+            i++;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
