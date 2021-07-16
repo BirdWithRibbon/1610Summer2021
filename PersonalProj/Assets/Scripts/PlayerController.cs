b93db7d3c6a996fe8be084b5296360a05e069bef
James Spencer <10483217@my.uvu.edu>
2021-07-15 19:26:06 -0600

    Adding player code.

---
+++ PersonalProj/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,131 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Player Statistics
+    public float jumpPower = 28.0f;
+    public float horizontalSpeed = 12.0f;
+    public float gravMod = 4.0f;
+    public float airControl = 1.0f; //Falling behavior does not work so does not alter.
+    public bool isOnGround = false;
+    public bool isFalling = true;
+
+    //These are inputs
+    private float horizontalInput;
+    private float jumpInput; //Weird choice of using gamepad's topmost face button.
+
+
+    //Game parameters
+    public float stageSideLimit = 20.0f; //When wrapping around sides.
+    public float groundFloor = -20.0f; //When falling off stage.
+    private Vector3 lastPosition;
+    private Vector3 currentPosition;
+
+    //Components
+    private Rigidbody playerRb;
+    private AudioSource playerAudio;
+    public AudioClip jumpSound;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.gravity *= gravMod;
+        playerAudio = GetComponent<AudioSource>();
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get current position for future reference.
+        currentPosition = transform.position;
+
+        //Harvest current inputs
+        horizontalInput = Input.GetAxis("Horizontal");
+        jumpInput = Input.GetAxis("Jump");
+
+        //Move direction based on aircontrol. Uses worldspace.
+        if (isFalling)
+        {
+            transform.Translate(horizontalInput * Time.deltaTime * horizontalSpeed, 0, 0, Space.World);
+        }
+        else
+        {
+            transform.Translate(horizontalInput * Time.deltaTime * horizontalSpeed * airControl, 0, 0, Space.World);
+        }
+        
+        
+        //Rotate object based on Input
+        if (horizontalInput != 0)
+        {
+            if (horizontalInput > 0)
+            {
+                transform.rotation = Quaternion.Euler(0, 90, 0);
+            }
+            else if(horizontalInput < 0)
+            {
+                transform.rotation = Quaternion.Euler(0, -90, 0);
+            }
+        }
+
+        //Jump if on ground
+        if (jumpInput != 0 && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            isOnGround = false;
+            //playerAnim.SetTrigger("Jump_trig");
+            //dirtParticle.Stop();
+            //playerAudio.PlayOneShot(jumpSound, 1.0f);
+        }
+
+        //Jump through platforms the lazy way.
+        if (playerRb.velocity.y > 0)
+        {
+            playerRb.detectCollisions = false;
+        }
+        else
+        {
+            playerRb.detectCollisions = true;
+        }
+
+        //Wrapping around stage behavior
+        if (currentPosition.x > stageSideLimit)
+        {
+            //Vector3 temp = new Vector3(stageSideLimit * 2 - 1, 0, 0);
+            //transform.position -= temp;
+            transform.position = new Vector3(-stageSideLimit + 2, currentPosition.y, currentPosition.z);
+        }
+        else if (currentPosition.x < -stageSideLimit)
+        {
+            //Vector3 temp = new Vector3(stageSideLimit * 2 - 1, 0, 0);
+            //transform.position += temp;
+            transform.position = new Vector3(stageSideLimit - 2, currentPosition.y, currentPosition.z);
+        }
+
+        //Collect the player if they fall off stage.
+        if (currentPosition.y < groundFloor)
+        {
+            transform.position = new Vector3(0, 0, 0);
+            playerRb.velocity = new Vector3(0, 0, 0);
+        }
+
+    }
+    
+    //Colliding with objects!
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Check to see if player is on ground. Currently causes issues when hitting from below or when colliding with the side of a ground object.
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+    }
+    
+    //When leaving a shelf
+    void OnCollisionExit(Collision collision)
+    {
+        isOnGround = false;
+    }
+}
